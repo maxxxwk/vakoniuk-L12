48d7e03b3e1f8013f3265958fb19fad741f0ad6d
Maxym Vakonyuk <makswk04@gmail.com>
2021-01-04 23:39:35 +0200

    Added Alert Dialog

--- app/src/main/java/com/maxxxwk/dialogs/MainActivity.kt
+++ app/src/main/java/com/maxxxwk/dialogs/MainActivity.kt
@@ -2,10 +2,44 @@ package com.maxxxwk.dialogs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.maxxxwk.dialogs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setupBinding()
+        setupListeners()
+    }
+
+    private fun setupBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun setupListeners() {
+        binding.buttonShowAlertDialog.setOnClickListener {
+            showAlertDialog()
+        }
+    }
+
+    private fun showAlertDialog() {
+        AlertDialog.Builder(this)
+            .setTitle(R.string.alert_dialog_title)
+            .setMessage(R.string.alert_dialog_message)
+            .setIcon(R.drawable.ic_launcher_foreground)
+            .setPositiveButton(R.string.alert_dialog_positive_button_text) { dialog, which ->
+                val text = "${getString(R.string.alert_dialog_positive_button_text)} clicked"
+                Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+            }
+            .setNegativeButton(R.string.alert_dialog_negative_button_text) { dialog, which ->
+                val text = "${getString(R.string.alert_dialog_negative_button_text)} clicked"
+                Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+            }
+            .show()
     }
 }
